6dbc7b153b8c6077b149ddb1a388c23a8306693f
Musso <hyukjun0216@gmail.com>
2024-03-07 00:16:59 +0900

    jwt강의내용

---
+++ springsecSection9/src/main/java/com/eazybytes/constants/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.eazybytes.constants;
+
+
+public interface SecurityConstants {
+    public static final String JWT_KEY = "asdzxfnnsfklnasklrn12alsASGASD125ASGN";
+    public static final String JWT_HEADER = "Authorization";
+
+}
